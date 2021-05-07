5518087d8fee62d279ee1a529e55835e502429f7
toshichanapp <toshichanapp@gmail.com>
2021-05-07 17:34:25 +0900

    item.nameによる分岐を一つにする

--- GildedRose/gilded_rose.rb
+++ GildedRose/gilded_rose.rb
@@ -6,53 +6,66 @@ class GildedRose
 
   def execute
     @items.each do |item|
-      update_quality(item)
+      case item.name
+      when 'Aged Brie'
+        update_sell_in(item)
+        update_aged_brie_quality(item)
+      when 'Backstage passes to a TAFKAL80ETC concert'
+        update_sell_in(item)
+        update_backstage_quality(item)
+      when 'Sulfuras, Hand of Ragnaros'
+      else
+        update_sell_in(item)
+        update_quality(item)
+      end
     end
   end
 
-  def update_quality(item)
-    if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-      if item.quality > 0
-        if item.name != "Sulfuras, Hand of Ragnaros"
-          item.quality = item.quality - 1
-        end
-      end
-    else
+  private
+
+  def update_sell_in(item)
+    item.sell_in = item.sell_in - 1
+  end
+
+  def update_aged_brie_quality(item)
+    if item.quality < 50
+      item.quality = item.quality + 1
+    end
+
+    if item.sell_in < 0
       if item.quality < 50
         item.quality = item.quality + 1
-        if item.name == "Backstage passes to a TAFKAL80ETC concert"
-          if item.sell_in < 11
-            if item.quality < 50
-              item.quality = item.quality + 1
-            end
-          end
-          if item.sell_in < 6
-            if item.quality < 50
-              item.quality = item.quality + 1
-            end
-          end
-        end
       end
     end
-    if item.name != "Sulfuras, Hand of Ragnaros"
-      item.sell_in = item.sell_in - 1
-    end
-    if item.sell_in < 0
-      if item.name != "Aged Brie"
-        if item.name != "Backstage passes to a TAFKAL80ETC concert"
-          if item.quality > 0
-            if item.name != "Sulfuras, Hand of Ragnaros"
-              item.quality = item.quality - 1
-            end
-          end
-        else
-          item.quality = item.quality - item.quality
-        end
-      else
+  end
+
+  def update_backstage_quality(item)
+    if item.quality < 50
+      item.quality = item.quality + 1
+      if item.sell_in < 11
         if item.quality < 50
           item.quality = item.quality + 1
         end
       end
+      if item.sell_in < 6
+        if item.quality < 50
+          item.quality = item.quality + 1
+        end
+      end
+    end
+
+    if item.sell_in < 0
+      item.quality = item.quality - item.quality
+    end
+  end
+
+  def update_quality(item)
+    if item.quality > 0
+      item.quality = item.quality - 1
+    end
+
+    if item.sell_in < 0
+      item.quality = item.quality - item.quality
     end
   end
 end
