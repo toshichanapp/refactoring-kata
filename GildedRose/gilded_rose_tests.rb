f602a4aa1ea6cfbbcfeaeb7278ef9c2090b8e9b7
toshichanapp <toshichanapp@gmail.com>
2021-05-07 16:29:38 +0900

    add test

--- GildedRose/gilded_rose_tests.rb
+++ GildedRose/gilded_rose_tests.rb
@@ -4,9 +4,24 @@ require 'test/unit'
 class TestUntitled < Test::Unit::TestCase
 
   def test_foo
-    items = [Item.new("foo", 0, 0)]
+    foo =  Item.new("foo", 1, 1)
+    aged_brie = Item.new("Aged Brie", 1, 1)
+    sulfuras = Item.new("Sulfuras, Hand of Ragnaros", 1, 80)
+    backstage_passes = Item.new("Backstage passes to a TAFKAL80ETC concert", 1, 10)
+    expired_backstage_passes = Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 10)
+    items = [foo, aged_brie, sulfuras, backstage_passes]
+
     GildedRose.new(items).update_quality()
-    assert_equal items[0].name, "foo"
+    assert_equal foo.sell_in, 0
+    assert_equal foo.quality, 0
+    assert_equal aged_brie.sell_in, 0
+    assert_equal aged_brie.quality, 2
+    assert_equal sulfuras.sell_in, 1
+    assert_equal sulfuras.quality, 80
+    assert_equal backstage_passes.sell_in, 0
+    assert_equal backstage_passes.quality, 13
+    assert_equal expired_backstage_passes.sell_in, -1
+    assert_equal expired_backstage_passes.quality, 0
   end
 
 end
