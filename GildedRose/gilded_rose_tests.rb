9b8aa1022f7eb58ffd7a22a7402154afa1cbecc9
toshichanapp <toshichanapp@gmail.com>
2021-05-07 14:42:26 +0900

    fix test

--- GildedRose/gilded_rose_tests.rb
+++ GildedRose/gilded_rose_tests.rb
@@ -1,4 +1,4 @@
-require File.join(File.dirname(__FILE__), 'gilded_rose')
+require_relative 'gilded_rose.rb'
 require 'test/unit'
 
 class TestUntitled < Test::Unit::TestCase
@@ -6,7 +6,7 @@ class TestUntitled < Test::Unit::TestCase
   def test_foo
     items = [Item.new("foo", 0, 0)]
     GildedRose.new(items).update_quality()
-    assert_equal items[0].name, "fixme"
+    assert_equal items[0].name, "foo"
   end
 
 end
